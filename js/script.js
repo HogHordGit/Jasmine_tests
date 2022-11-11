4d72d3fefbcf9cf0e39397ae4d0b7cfb5430768f
HogHordGit <hoghord@gmail.com>
2022-11-12 01:26:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function ageClassification(num) {
+    return num > 0
+        ? num > 24
+            ? num > 44
+                ? num > 65
+                    ? num > 75
+                        ? num > 90
+                            ? num > 122
+                                ? null
+                                : 'долгожители'
+                            : 'старческий возраст'
+                        : 'пожилой возраст'
+                    : 'средний возраст'
+                : 'молодой возраст'
+            : 'детский возраст'
+        : null;
+}
+
+function weekFn(num) {
+    let str;
+  
+    switch (num) {
+      case 1:
+        str = 'Понедельник';
+        break;
+      case 2:
+        str = 'Вторник';
+        break;
+      case 3:
+        str = 'Среда';
+        break;
+      case 4:
+        str = 'Четверг';
+        break;
+      case 5:
+        str = 'Пятница';
+        break;
+      case 6:
+        str = 'Суббота';
+        break;
+      case 7:
+        str = 'Воскресенье';
+        break;
+      default:
+        str = null;
+    }
+  
+    return str;
+}

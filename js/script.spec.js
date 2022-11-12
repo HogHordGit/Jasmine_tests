4ea50813a19f1b2fb0fabfec4c1c56b46e7b853c
Vladymyr <hoghord@gmail.com>
2022-11-12 19:51:09 +0200

    change script.spec.js

--- js/script.spec.js
+++ js/script.spec.js
@@ -1,102 +1,86 @@
 describe("Тестирование функции ageClassification(num)",() => {
-    const testArr = [-1, 1, 24, 24.01, 44, 44.01, 65, 65.1, 75, 75.01, 90, 90.01, 122, 122.01, 150];
-
-    let m;
-    let i = 0;
-
-    beforeEach(() => {
-        m = ageClassification(testArr[i++]);
-    });
 
     it("Проверим num = -1, должно вернуть null",() => {
-        expect(m).toBe(null);
+        expect(ageClassification(-1)).toBe(null);
     });
 
     it("Проверим num = 1, должно вернуть 'детский возраст'",() => {
-        expect(m).toBe("детский возраст");
+        expect(ageClassification(1)).toBe("детский возраст");
     });
 
     it("Проверим num = 24, должно вернуть 'детский возраст'",() => {
-        expect(m).toBe("детский возраст");
+        expect(ageClassification(24)).toBe("детский возраст");
     });
 
     it("Проверим num = 24.01, должно вернуть 'молодой возраст'",() => {
-        expect(m).toBe("молодой возраст");
+        expect(ageClassification(24.01)).toBe("молодой возраст");
     });
 
     it("Проверим num = 44, должно вернуть 'молодой возраст'",() => {
-        expect(m).toBe("молодой возраст");
+        expect(ageClassification(44)).toBe("молодой возраст");
     });
 
     it("Проверим num = 44.01, должно вернуть 'cредний возраст'",() => {
-        expect(m).toBe("средний возраст");
+        expect(ageClassification(44.01)).toBe("средний возраст");
     });
 
     it("Проверим num = 65 , должно вернуть 'cредний возраст'",() => {
-        expect(m).toBe("средний возраст");
+        expect(ageClassification(65)).toBe("средний возраст");
     });
 
     it("Проверим num = 65.1, должно вернуть 'пожилой возраст'",() => {
-        expect(m).toBe("пожилой возраст");
+        expect(ageClassification(65.1)).toBe("пожилой возраст");
     });
 
     it("Проверим num = 75, должно вернуть 'пожилой возраст'",() => {
-        expect(m).toBe("пожилой возраст");
+        expect(ageClassification(75)).toBe("пожилой возраст");
     });
 
     it("Проверим num = 75.01, должно вернуть 'старческий возраст'",() => {
-        expect(m).toBe("старческий возраст");
+        expect(ageClassification(75.01)).toBe("старческий возраст");
     });
 
     it("Проверим num = 90 , должно вернуть 'старческий возраст'",() => {
-        expect(m).toBe("старческий возраст");
+        expect(ageClassification(90)).toBe("старческий возраст");
     });
 
     it("Проверим num = 90.01, должно вернуть 'долгожители'",() => {
-        expect(m).toBe("долгожители");
+        expect(ageClassification(90.01)).toBe("долгожители");
     });
 
     it("Проверим num = 122, должно вернуть 'долгожители'",() => {
-        expect(m).toBe("долгожители");
+        expect(ageClassification(122)).toBe("долгожители");
     });
 
     it("Проверим num = 122.01  должно вернуть null",() => {
-        expect(m).toBe(null);
+        expect(ageClassification(122.01)).toBe(null);
     });
 });
 
 describe("Тестирование функции weekFn(num)",() => {
-    const testArr = [1, 3, 7, 9, 1.5, "2"];
-
-    let m;
-    let i = 0;
-
-    beforeEach(() => {
-        m = weekFn(testArr[i++]);
-    });
 
     it("Проверим num = 1, должно вернуть 'Понедельник'",() => {
-        expect(m).toBe('Понедельник');
+        expect(weekFn(1)).toBe('Понедельник');
     });
 
     it("Проверим num = 3, должно вернуть 'Среда'",() => {
-        expect(m).toBe('Среда');
+        expect(weekFn(3)).toBe('Среда');
     });
 
     it("Проверим num = 7, должно вернуть 'Воскресенье'",() => {
-        expect(m).toBe('Воскресенье');
+        expect(weekFn(7)).toBe('Воскресенье');
     });
 
     it("Проверим num = 9, должно вернуть null",() => {
-        expect(m).toBe(null);
+        expect(weekFn(9)).toBe(null);
     });
 
     it("Проверим num = 1.5, должно вернуть null",() => {
-        expect(m).toBe(null);
+        expect(weekFn(1.5)).toBe(null);
     });
 
     it("Проверим num = '2', должно вернуть null",() => {
-        expect(m).toBe(null);
+        expect(weekFn("2")).toBe(null);
     });
 });
 
